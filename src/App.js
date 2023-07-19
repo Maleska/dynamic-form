3898d580f253af044f7423096b7eefa059e36f7f
Alex Matias <alejandro.matias@hotmail.com>
2023-07-19 11:50:20 -0600

    add navbar menu

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import { Button } from 'react-bootstrap';
+import NavbarMenu from './components/NavbarMenu';
+
+function App() {
+  return (
+    <div >
+    <NavbarMenu />
+    </div>
+  );
+}
+
+export default App;
